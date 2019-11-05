d4b6c007194a91d401740d8fd8d3c468c52dd75a
Obaro Johnson <obaro.johnson@servotestsystems.com>
2019-11-05 11:39:29 +0000

    Server working

--- file_server/server.py
+++ file_server/server.py
@@ -8,11 +8,18 @@
  to send update asynchrously to and from the client and the server
 """
 
-from socket import socket;
+import socket;
 from threading import Thread, Lock;
 import time;
+import os;
+import signal; # Handle system signals
 
-DEFAULT_PORT_ADDRESS  = 3607;
+DEFAULT_PORT_ADDRESS  = 36061;
+
+class SignalHandler(object):
+    def __init__(self):
+        super().__init__();
+        
 
 class DaemonServer(object):
 
@@ -23,8 +30,28 @@ class DaemonServer(object):
         self.__RunThread    = None;
         self.__IsRunning    = False;
         self.__CanStart     = False;
-        self.__socket       = socket();
+        self.__Socket       = None;
+        self.__HostAddress  = None;
+        self.__Clients      = list();
         
+    @property
+    def Clients(self):
+        return self.__Clients;
+        
+    @property
+    def HostAddress(self):
+        return self.__HostAddress;
+    
+    @HostAddress.setter
+    def HostAddress(self, hostaddress):
+        if(type(hostaddress) != str):
+            raise ValueError("@Invalid hostaddress provided");
+        if(self.IsRunning):
+            print("Unable to set the host address while server is running");
+            return ;
+        else:
+            self.__HostAddress = hostaddress;
+    
     @property
     def Port(self):
         return self.__Port;
@@ -44,9 +71,16 @@ class DaemonServer(object):
         if(self.IsRunning == True):
             print("Unable to start server when server is already started");
             return ;
-        self.__RunThread = Thread(target  = self.__InBackground);
-        self.__RunThread.daemon  = True;
-        self.__RunThread.start();
+        if(self.__Socket == None):
+            # Create TCP socket
+            self.__Socket  = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+            if(self.HostAddress == None):
+                self.HostAddress  = socket.gethostbyname(socket.gethostname());
+            self.__Socket.bind((self.HostAddress, self.Port));
+            self.__Socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.__RunThread = Thread(target  = self.__InBackground);
+            self.__RunThread.daemon  = True;
+            self.__RunThread.start();
         
     @property
     def IsRunning(self):
@@ -76,10 +110,25 @@ class DaemonServer(object):
         self.__StartLocker.release();
         self.__IsRunning  = True;
         self.__CanStart   = False;
-        print("Running Worlking");
-        while(self.IsRunning):
+        print("Server started at : {0}:{1}".format(self.HostAddress, self.Port));
+        while(self.IsRunning and (self.__Socket != None)):
             print("Server listening will be here to handler clients");
-            pass;
+            try:
+                # Listen and handle shake with clients
+                self.__Socket.listen(1);
+                client  = self.__Socket.accept(); # return a client stream to write and read from;
+                ## Register the client and listen to write and read events for the client;
+                self.Clients.append(client);
+                print(client);
+            except Exception as err:
+                # Raise error events and terminate the server;
+                if(self.IsRunning):
+                    self.Close();
+            finally:
+                #Remove all client and send and if possible send them
+                # A server shutdow message
+                print("Finally");
+                pass;
 
         
         pass;
@@ -90,6 +139,12 @@ class DaemonServer(object):
             if(self.__IsRunning):
                 self.__IsRunning  = False;
                 self.__RunThread.join(1);
+                
+                if(self.__Socket != None):
+                    # Close all client connections;
+                    for client in self.Clients:
+                        client.close();
+                    self.__Socket.close();
                
         except Exception as err:
             print(err);
@@ -104,15 +159,12 @@ class DaemonServer(object):
 
 if(__name__ =="__main__"):
     server = DaemonServer();
-    server.Port  = 5617;
     server.Start();
 
-    time.sleep(1);
-    server.Stop();
-    print("Start again");
-    time.sleep(0.5);
-    server.IsRunning  = True;
+   
  
-    time.sleep(1);
-    server.Stop();
+    while(server.IsRunning):
+        pass;
+    server.Close();
+        
 
