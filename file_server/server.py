9b539a7e8fc7fd23aaf2acd0ae5d539493262c0e
johnson obaro <johnson.obaro@hotmail.com>
2020-05-30 19:44:12 +0000

    Remove server dependency to a single function socket only

--- file_server/server.py
+++ file_server/server.py
@@ -16,13 +16,84 @@ import signal;
 import io;
 from events         import BaseObject , Event, EventHandler;
 from constants      import *;
-from clientmanager  import ClientManager;
-from client         import Client;
-from request        import Request;
+
+class ISocketServer(object):
+
+    def Start(self):
+        raise NotImplementedError("@Start: method must be implemented");
+
+    def Stop(self):
+        raise NotImplementedError("@Stop: method must be implemented");
 
 
+    @property
+    def HostAddress(self):
+        raise NotImplementedError("@HostAddress: property must be implemented");
+
+
+class SocketCloseException(Exception):
+
+    def __init__(self, inner_exception:Exception):
+        if(isinstance(inner_exception , Exception) is not True):
+            raise TypeError("Expecting an exception object as first parameter");        
+        super().__init__('close_exception:');
+        self.__InnerException = inner_exception;
+        
+    @property
+    def InnerException(self):
+        return self.__InnerException;
+
+class SocketStartEvent(Event):
+
+    def __init__(self, source : object):
+        if(isinstance(source , ISocketServer) is not True):
+            raise TypeError("SocketStartEvent: expecting a ISocketServer object");
+        super().__init__('socket.start.event');
+        self.__Source  =  source;
+
+    @property
+    def Socket(self):
+        return self.__Source;
+        
+        
+class SocketCloseEvent(Event):
+
+    def __init__(self, exception: SocketCloseException):
+        
+        if(isinstance(exception, SocketCloseException) is not True):
+            raise TypeError("@SocketCloseEvent: expecting an SocketCloseException object ");
+        super().__init__("socket.close.event");
+        self.__Exception = exception;
+
+    @property
+    def Exception(self):
+        return self.__Exception;
+
+class SocketAcceptedEvent(Event):
+
+    def __init__(self, source :ISocketServer, client: socket.socket, ipaddress:str):
+        if(isinstance(source,ISocketServer) is not True):
+            raise TypeError("@SocketAcceptedEvent : expecting paremeter 2 to be a ISocketServer object");
+        super().__init__("socket.accept.event");
+        self.__Source    =  source;
+        self.__Socket    =  client;
+        self.__IpAddress =  ipaddress;
+
+    @property
+    def Socket(self):
+        return self.__Socket;
     
-class DaemonServer(object):
+    @property
+    def IpAddress(self):
+        return self.__IpAddress;
+
+    @property
+    def Source(self):
+        return self.__Source;
+        
+        
+    
+class SocketServer(ISocketServer):
     
     def __init__(self, **kwargs):
         self.__Port  = kwargs['port'] if(('port' in kwargs) and (type(kwargs['port']) == int)) else DEFAULT_PORT_ADDRESS;
@@ -31,28 +102,38 @@ class DaemonServer(object):
         self.__IsRunning    = False;
         self.__Socket       = None;
         self.__HostAddress  = None;
-        self.__BackLogConnections =  DEFAULT_BACK_LOG_CONNECTION ;
-        self.__Clients              = ClientManager();
-        self.__ThreadPoles          = list();
-        self.__ClientConnected      = EventHandler();
-        self.__ClientDisconnected   = EventHandler();
+        self.__BackLogConnections   = DEFAULT_BACK_LOG_CONNECTION ;
+        self.__Accepted             = EventHandler();
+        self.__Closed               = EventHandler();
+        self.__Started              = EventHandler();
+
+    @property
+    def Started(self):
+        return self.__Started;
+
+    @Started.setter
+    def Started(self, handler):
+        if(handler == self.__Started):
+            self.__Started  =  handler;
+
+    @property
+    def Closed(self):
+        return self.__Closed;
+
+    @Closed.setter
+    def Closed(self, handler):
+        if(handler == self.__Closed):
+            self.__Closed =  handler;
+     
         
     @property
-    def ClientConnected(self):
-        return self.__ClientConnected;
+    def Accepted(self):
+        return self.__Accepted;
 
-    @ClientConnected.setter
-    def ClientConnected(self, handler):
+    @Accepted.setter
+    def Accepted(self, handler):
         if(isinstance(handler , EventHandler)):
-            self.__ClientConnected   =  handler;
-    @property
-    def ClientDisconnected(self):
-        return self.__ClientDisconnected;
-
-    @ClientDisconnected.setter
-    def ClientDisconnected(self, handler):
-        if(isinstance(handler , EventHandler)):
-            self.__ClientDisconnected   =  handler;
+            self.__Accepted   =  handler;
 
     @property
     def BackLogConnections(self):
@@ -63,10 +144,6 @@ class DaemonServer(object):
         if type(value) is not int:
             raise TypeError("@Backlog : expecting an integer value");
         self.__BackLogConnections = value;
-        
-    @property
-    def Clients(self):
-        return self.__Clients;
         
     @property
     def HostAddress(self):
@@ -131,52 +208,35 @@ class DaemonServer(object):
     def __InBackground(self):
         self.__StartLocker.release();
         self.__IsRunning  = True;
-        print("Server started at : {0}:{1}".format(self.HostAddress, self.Port));
-        
+        # Tell the users that the socket has be started.
+        if(self.Started != None):
+            self.Started(SocketStartEvent(self));
+         
         while(self.IsRunning):           
-            while(self.__Socket != None):
+            if(self.__Socket != None):
                 try:
                     self.__Socket.listen(self.BackLogConnections);
                     socket, address  = self.__Socket.accept();
-                    
                     if(socket != None):
-                        client  = Client(socket=socket , timeout=0)
-                        newConnetionThread          =  Thread(target= self.__HandleNewConnection, args=(client,));
-                        newConnetionThread.daemon   = False;
-                        client.Thread               = newConnetionThread;
-                        newConnetionThread.start();
-                        self.__ThreadPoles.append(newConnetionThread);
+                        if(self.Accepted != None):
+                            self.Accepted(SocketAcceptedEvent(self, socket, address));                       
+                        pass;
+                      
                 except Exception as err:
-                    if(self.IsRunning):
-                        self.Stop();
-                    raise err;
+                    self.Stop();
+                    if(self.Closed != None):
+                        self.Closed(SocketCloseEvent(SocketCloseException(err)));                    
                 finally:
                     pass;
         self.Stop();
 
-    def __HandleNewConnection(self, client):
-        try:
-            if(client != None):
-                self.Clients.Add(client);
-                while(client.IsOpened):
-                    message  = client.Read();
-                    print(message);
-                    if not message :
-                        client.Close();
-                    else:
-                        request  =  Request.Parse(message);
-                        print(request);
-                print("Closed");
-        except Exception as err:
-            client.Close();
-            raise err;
+   
 
     def Stop(self):
         self.__StartLocker.acquire();
         try:
-            if(self.IsRunning):               
+            if(self.IsRunning):
                 if(self.__RunThread.is_alive()):
-                    self.__CloseAllClient();
                     self.__IsRunning  = False;
                     self.__RunThread.join(WAIT_THREAD_INTERVAL);
                 if(self.__Socket != None):                   
@@ -186,33 +246,47 @@ class DaemonServer(object):
             print(err);
         finally:
             self.__RunThread  = None;
-        self.__StartLocker.release();
+            self.__StartLocker.release();
 
 
-    def __CloseAllClient(self):
-       self.Clients.Clear();
+
 
 
     
 if(__name__ =="__main__"):
+    import time;
+    def OnStarted(evt):
+       print("Server started at : {0}:{1}".format(evt.Socket.HostAddress, evt.Socket.Port));
+
+    def OnAccepted(evt):
+        print("Connection Request Accepted");
+
+    def OnClosed(evt):
+        print(evt.InnerException);
+        
+       
     def run(port):
-        server = DaemonServer(port = port);
+        server = SocketServer(port = port);
+        server.Started  += OnStarted;
+        server.Accepted += OnAccepted;
+        server.Closed  += OnClosed;
         server.Start();
         try:
+            print("Start...");
+            time.sleep(2);
             while(server.IsRunning):
                 
                 pass;
+            print("Completed");
         except Exception as err:
             print("Stop unexceptedly - {0}".format(err));
         finally:
             server.Stop();
+
+    run(8082);
         
-    def start_server_at(port):
-        t  =  Thread(target = run, args=(port,));
-        t.start();
-    start_server_at(8082);
+
     
-    while(True):
-       pass;
+  
         
 
